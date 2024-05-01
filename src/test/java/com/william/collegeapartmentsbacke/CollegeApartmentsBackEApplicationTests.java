2f9fddfb330b287d04fc6de4219608c85e90df5b
William <2092107214@qq.com>
2024-05-01 16:58:24 +0800

    init Project

---
+++ src/test/java/com/william/collegeapartmentsbacke/CollegeApartmentsBackEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.collegeapartmentsbacke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeApartmentsBackEApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
