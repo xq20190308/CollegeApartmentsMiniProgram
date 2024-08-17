1886601bc5f4d0abf5c5817d06d5e4ac83424678
caoxiaoyuyuyuyuyu <1844118046@qq.com>
2024-08-17 12:14:40 +0800

    添加了响应头，但是资源服务器访问不了

---
+++ src/main/java/com/william/collegeapartmentsbacke/controller/FileController.java
@@ -0,0 +1,29 @@
+package com.william.collegeapartmentsbacke.controller;
+
+import com.william.collegeapartmentsbacke.common.annotations.NoNeedLogin;
+import com.william.collegeapartmentsbacke.service.FileService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+//文件上传
+@Slf4j
+@RestController
+@RequestMapping(value = "/upload")
+public class FileController {
+
+    @Autowired
+    FileService fileService;
+
+    @GetMapping(value = "/delete")
+    @NoNeedLogin
+    public String deleteFile() {
+        return "delet";
+    }
+}
+
